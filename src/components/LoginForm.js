d41e06a7653c859d0bac064422c51aa43e90870f
Gary Ditsch <garyditsch@gmail.com>
2017-08-16 10:09:21 -0400

    login form complete

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
 import { connect } from 'react-redux';
-import { Card, CardSection, Input, Button } from './common';
+import { Card, CardSection, Input, Button, Spinner } from './common';
 import { emailChanged, passwordChanged, loginUser } from '../actions';
 
 class LoginForm extends Component {
@@ -16,6 +16,17 @@ class LoginForm extends Component {
     onButtonPress() {
         const { email, password } = this.props;
         this.props.loginUser({ email, password });
+    }
+
+    renderButton() {
+        if (this.props.loading) {
+            return <Spinner size="large" />;
+        }
+        return (
+            <Button onPress={this.onButtonPress.bind(this)}>
+                Login
+            </Button>
+        );
     }
 
     render() {
@@ -43,11 +54,7 @@ class LoginForm extends Component {
                     {this.props.error}
                 </Text>
                 <CardSection>
-                    <Button
-                        onPress={this.onButtonPress.bind(this)}
-                    >
-                        Login
-                    </Button>
+                    {this.renderButton()}
                 </CardSection>
             </Card>
         );
@@ -63,8 +70,8 @@ const styles = {
 }
 
 const mapStateToProps = ({ auth }) => {
-    const { email, password, error } = auth;
-    return { email, password, error };
+    const { email, password, error, loading } = auth;
+    return { email, password, error, loading };
 };
 
 export default connect(mapStateToProps, { emailChanged, passwordChanged, loginUser })(LoginForm);

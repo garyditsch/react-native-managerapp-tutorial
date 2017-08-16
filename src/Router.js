776ce826e929c50d67712a5e4ae63ee8bd0e58b8
Gary Ditsch <garyditsch@gmail.com>
2017-08-16 15:31:08 -0400

    adding buckets to navigation

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Scene, Router } from 'react-native-router-flux';
+import { Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
+import EmployeeCreate from './components/EmployeeCreate';
 
 const RouterComponent = () => {
     return (
@@ -11,11 +13,17 @@ const RouterComponent = () => {
             </Scene>
             <Scene key="main">
                 <Scene
-                    onRight={() => console.log('right')}
+                    onRight={() => Actions.employeeCreate() }
                     rightTitle="Add"
                     key="employeelist"
                     component={EmployeeList}
                     title="Employees"
+                    initial
+                />
+                <Scene
+                    key="employeeCreate"
+                    component={EmployeeCreate}
+                    title="Create Employee"
                 />
             </Scene>
         </Router>

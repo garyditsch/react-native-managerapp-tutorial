53021847a06bf682bc458edfb0271f8e5366c6ce
Gary Ditsch <garyditsch@gmail.com>
2017-08-19 11:29:04 -0400

    can now update employee

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Card, CardSection, Button } from './common';
 import EmployeeForm from './EmployeeForm';
-import { employeeUpdate } from '../actions';
+import { employeeUpdate, employeeSave } from '../actions';
 
 class EmployeeEdit extends Component {
     componentWillMount() {
@@ -14,7 +14,8 @@ class EmployeeEdit extends Component {
 
     onButtonPress() {
         const { name, phone, shift } = this.props;
-        console.log(name, phone, shift);
+        {this.props.employeeSave({ name, phone, shift, uid: this.props.employee.uid })};
+        
     }
 
     render() {
@@ -37,4 +38,4 @@ const mapStateToProps = (state) => {
     return { name, phone, shift };
 }
 
-export default connect(mapStateToProps, { employeeUpdate })(EmployeeEdit);
+export default connect(mapStateToProps, { employeeUpdate, employeeSave })(EmployeeEdit);

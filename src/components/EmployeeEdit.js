43cd1173ebaeaa4347b5758def2e1c5bc6ade8ef
Gary Ditsch <garyditsch@gmail.com>
2017-08-19 11:43:33 -0400

    adding text feature and confirm modal

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Communications from 'react-native-communications';
 import { Card, CardSection, Button } from './common';
 import EmployeeForm from './EmployeeForm';
 import { employeeUpdate, employeeSave } from '../actions';
@@ -15,7 +16,11 @@ class EmployeeEdit extends Component {
     onButtonPress() {
         const { name, phone, shift } = this.props;
         {this.props.employeeSave({ name, phone, shift, uid: this.props.employee.uid })};
-        
+    }
+
+    onTextPress() {
+        const { phone, shift } = this.props;
+        Communications.text(phone, `Your upcoming shirt is on ${shift}`)
     }
 
     render() {
@@ -25,6 +30,11 @@ class EmployeeEdit extends Component {
                 <CardSection>
                     <Button onPress={this.onButtonPress.bind(this)}>
                         Save Changes
+                    </Button>
+                </CardSection>
+                <CardSection>
+                    <Button onPress={this.onTextPress.bind(this)}
+                        Text Schedule
                     </Button>
                 </CardSection>
             </Card>
